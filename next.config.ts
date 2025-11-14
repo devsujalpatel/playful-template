be870f3e577e3b7e6dd516e29361429598e2631e
devsujalpatel <sujal7455@gmail.com>
2025-11-15 00:41:07 +0530

    Erro fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
-const withMDX = require("@next/mdx")();
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-module.exports = withMDX(nextConfig);
+export default nextConfig;
